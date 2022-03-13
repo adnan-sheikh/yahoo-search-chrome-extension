3e4eaa8fe033d50c73443a872d9b000f36a51957
adnan <mechcoder@pop-os.localdomain>
2022-03-14 01:18:13 +0530

    setup omnibox

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,11 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension is installed successfully!");
 });
+
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  console.log(text);
+  suggest([
+    { content: "hi", description: "this is the first suggestion" },
+    { content: "hi again", description: "this is the second suggestion" },
+  ]);
+});
--- src/contentScript.ts
+++
@@ -1,3 +0,0 @@
-window.onload = () => {
-  alert("Content script is running now!");
-};
