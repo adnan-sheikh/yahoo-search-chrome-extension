52aa84fb04138b39f2fd4176ce776ee9eac099a6
adnan <mechcoder@pop-os.localdomain>
2022-03-14 03:15:49 +0530

    wip: try adding module import

--- src/background.ts
+++ src/background.ts
@@ -1,18 +1,15 @@
 import SerpApi from "google-search-results-nodejs";
-
 const search = new SerpApi.GoogleSearch("secret_api_key");
+
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension is installed successfully!");
 });
 
-chrome.omnibox.onInputEntered.addListener((text, suggest) => {
-  const newUrl = `https://search.yahoo.com/search?p=${encodeURIComponent(
-    text
-  )}`;
-  fetch(newUrl).then((res) => {
-    console.log(res.body);
-  });
-  // .then((jsonRes) => {
-  //   console.log(jsonRes);
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  // const newUrl = `https://search.yahoo.com/search?p=${encodeURIComponent(
+  //   text
+  // )}`;
+  // fetch(newUrl).then((res) => {
+  console.log(text);
   // });
 });
