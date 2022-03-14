0a287ece6788a36039cdceb7bf274b598fbb2ee6
adnan <mechcoder@pop-os.localdomain>
2022-03-14 20:42:11 +0530

    add some static suggestions on omnibox input change

--- src/background.ts
+++ src/background.ts
@@ -4,11 +4,28 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.omnibox.setDefaultSuggestion({ description: "My Yahoo search!" });
 
-chrome.omnibox.onInputEntered.addListener((text, suggest) => {
+chrome.omnibox.onInputEntered.addListener((text) => {
   const urlToSearch = `https://search.yahoo.com/search?p=${encodeURIComponent(
     text
   )}`;
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.update(tabs[0].id!, { url: urlToSearch });
   });
+});
+
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  suggest([
+    {
+      content: `${text}1`,
+      description: `This description is related to ${text}1`,
+    },
+    {
+      content: `${text}2`,
+      description: `This description is related to ${text}2`,
+    },
+    {
+      content: `${text}3`,
+      description: `This description is related to ${text}3`,
+    },
+  ]);
 });
