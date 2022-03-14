85addc937e8c7a8ab83652ff92f7ea72bbaaf983
adnan <mechcoder@pop-os.localdomain>
2022-03-14 20:18:02 +0530

    perform yahoo search on input entered in omnibox;

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,12 @@
-import SerpApi from "google-search-results-nodejs";
-const search = new SerpApi.GoogleSearch("secret_api_key");
-
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension is installed successfully!");
 });
 
-chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-  // const newUrl = `https://search.yahoo.com/search?p=${encodeURIComponent(
-  //   text
-  // )}`;
-  // fetch(newUrl).then((res) => {
-  console.log(text);
-  // });
+chrome.omnibox.onInputEntered.addListener((text, suggest) => {
+  const urlToSearch = `https://search.yahoo.com/search?p=${encodeURIComponent(
+    text
+  )}`;
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.update(tabs[0].id!, { url: urlToSearch });
+  });
 });
--- src/google-search-results-nodejs.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "google-search-results-nodejs";
